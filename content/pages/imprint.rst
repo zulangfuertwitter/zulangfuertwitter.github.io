c47ed687359ef556966a1d9a703c0ac5fdef460e
zulangfuertwitter <zulangfuertwitter@mail.com>
2014-01-09 05:09:22 +0100

    first commit

---
+++ content/pages/imprint.rst
@@ -0,0 +1,10 @@
+Impressum
+=========
+.. |br| raw:: html
+
+   <br />
+
+Zu lang für Twitter |br|
+Heinrich-Hagens-Straße 23 |br|
+28207 Bremen |br|
+E-Mail: zulangfuertwitter@mail.com |br|
---
+++ pelicanconf.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'unknown'
+SITENAME = u'Zu lang für Twitter'
+SITESUBTITLE = u'Nachrichten mit Kommentaren, die mehr als 140 Zeichen lang sind'
+SITEURL = ''
+
+PATH = 'content'
+PAGE_DIR = 'pages'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'de'
+
+CSS_FILE = 'style.css'
+THEME = u'mytheme'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+DEFAULT_PAGINATION = 20
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
